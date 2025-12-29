eb9131afbd1186b0ebd765159b8cb7712c76ae3e
ppcykubpom <pichayut.tho@ku.th>
2025-12-29 17:05:12 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+/*
+loop statement
+while
+for
+*/
+let counter = 0;
+while (counter < 10) {
+    console.log('Hello World ');
+    //counter = counter + 1;
+    counter += 1;
+}
+for (let i = 0; i < 10; i++) {
+    console.log('Hello World from loop');
+}
